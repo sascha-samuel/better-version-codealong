90d9f6202b1377f3a3811d65f910c01881083c0f
sascha-samuel <s.muff@steiner-hitech.at>
2021-06-17 20:18:10 +0200

    Add new code

--- index.js
+++ index.js
@@ -2,4 +2,79 @@
 
 // New comment
 
+import { compareAsc, format } from 'date-fns';
 // Hello my friends
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
+format(new Date(2014, 1, 11), 'yyyy-MM-dd');
+//=> '2014-02-11'
+
+const dates = [
+  new Date(2015, 6, 2),
+  new Date(1987, 1, 11),
+  new Date(1989, 6, 10),
+];
+dates.sort(compareAsc);
+//=> [
+//   Wed Jan 15 1989 00:00:00,
+//   Mon Jul 10 1989 00:00:00,
+//   Sun Jul 02 1995 00:00:00
+// ]
+console.log('These are the dates:', dates);
+const got = require('got');
+
+(async () => {
+  const { body } = await got.post('https://httpbin.org/anything', {
+    json: {
+      hello: 'world',
+    },
+    responseType: 'json',
+  });
+
+  console.log(body.data);
+  //=> {hello: 'world'}
+})();
+const chalk = require('chalk');
+const log = console.log;
+
+// Combine styled and normal strings
+log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+
+// Compose multiple styles using the chainable API
+log(chalk.blue.bgRed.bold('Hello world!'));
+
+// Pass in multiple arguments
+log(chalk.blue('Hello', 'World!', 'Foo', 'bar', 'biz', 'baz'));
+
+// Nest styles
+log(chalk.red('Hello', chalk.underline.bgBlue('world') + '!'));
+
+// Nest styles of the same type even (color, underline, background)
+log(
+  chalk.green(
+    'I am a green line ' +
+      chalk.blue.underline.bold('with a blue substring') +
+      ' that becomes green again!',
+  ),
+);
+
+// ES2015 template literal
+log(`
+CPU: ${chalk.red('90%')}
+RAM: ${chalk.green('40%')}
+DISK: ${chalk.yellow('70%')}
+`);
+
+// ES2015 tagged template literal
+log(chalk`
+CPU: {red ${cpu.totalPercent}%}
+RAM: {green ${(ram.used / ram.total) * 100}%}
+DISK: {rgb(255,131,0) ${(disk.used / disk.total) * 100}%}
+`);
+
+// Use RGB colors in terminal emulators that support it.
+log(chalk.keyword('orange')('Yay for orange colored text!'));
+log(chalk.rgb(123, 45, 67).underline('Underlined reddish color'));
+log(chalk.hex('#DEADED').bold('Bold gray!'));
